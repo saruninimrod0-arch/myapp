9033639cb16f96a770b5dc9d1f236b3070d84453
saruninimrod0-arch <saruninimrod0@gmail.com>
2026-03-12 14:06:02 +0300

    add

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 
@@ -5,16 +6,71 @@ const Signup = () => {
 // initialize the hooks
 const [username, setUsername] = useState("");
 const [email, setEmail] = useState("");
-const [password, setPassword] = useState("")
+const [password, setPassword] = useState("");
 const [phone, setPhone] = useState("");
 
+// Define the three states an application will move to
+const [loading, setLoading] = useState("");
+const [success, setSuccess] = useState("");
+const [error, setError] =useState("");
+
+// Below is a function that will handle the submit action
+const handleSubmit = async(e) =>{
+  //Below we prevent our site from reloading
+   e.preventDefault()
+
+  // Updste our loading hooks with message that will be displayed to the users who are trying to register
+  setLoading("Please wait as registration is in progress...")
+
+  try{
+    // Create a form data object that will enable you to capture the four details entered on the form
+    const formdata = new FormData();
+
+    // Insert the four details (username, email, password, phone)interms of key - value pairs
+    formdata.append("username", username);
+    formdata.append("email", email);
+    formdata.append("password", password);
+    formdata.append("phone", phone);
+
+    // by use of axios, we can access the method post
+    const response =await axios.post("https://saruninimrod..alwaysdata.net/api/signup", formdata)
+
+    // set back the loading to default
+    setLoading("");
+
+    // just incase everything goes on well, update the success hook with a message
+    setSuccess(response.data.message)
+
+    // clear your hooks
+    setUsername("");
+    setEmail("");
+    setPassword("")
+    setPhone("");
+  }
+  catch(error){
+    // set the loading back to default
+    setLoading("");
+
+    // update the error hook with the massage given back from the response
+    setError(error.message)
+
+  }
+
+
+
+}
 
   return (
     <div className='row justify-content-center mt-4'>
     <div className="card col-md-6 shadow p-4">
         <h1 className='text-primary'>Sign Up</h1>
 
-        <form>
+        <h5 className='text-warning'>{loading}</h5>
+        <h3 className='text-success'>{success}</h3>
+        <h4 className='text-danger'>{error}</h4>
+
+
+        <form onSubmit={handleSubmit}>
           <input type="text"
           placeholder='Enter the Username'
           className='form-control'
@@ -43,21 +99,17 @@ const [phone, setPhone] = useState("");
           {/*{password}*/}
 
 
-          <input type="tel"
+          <input type="phone"
           placeholder='Enter the Mobile Phone number'
           className='form-control'
-          value={(e) => setPhone(e.target.value)}
-          required
-          minLength={10}/> <br />
-          {phone.length > 0 && phone.length <= 10 && (
-            <small style={{color: "red"}}>
-              phone number must be atleast 10 digits
-            </small>
-          )}
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          required/> <br />
+      
           
           {/*{phone}*/}
 
-          <input type="button" value="Signup" className='btn btn-primary' /> <br /> <br />
+          <input type="submit" value="Signup" className='btn btn-primary' /> <br /> <br />
           Already have an account? <Link to={'/signin'}>Signin</Link>
         </form>
     </div>
